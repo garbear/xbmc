d174101adb51fe700a43036e708a1af87f112142
Garrett Brown <themagnificentmrb@gmail.com>
2015-12-09 16:43:30 -0800

    [retroplayer] - (0002) Keyboard players

--- xbmc/games/controllers/windows/GUIConfigurationWizard.h
+++ xbmc/games/controllers/windows/GUIConfigurationWizard.h
@@ -42,7 +42,7 @@ namespace GAME
                                   protected CThread
   {
   public:
-    CGUIConfigurationWizard();
+    CGUIConfigurationWizard(bool bEmulation, unsigned int controllerNumber = 0);
 
     virtual ~CGUIConfigurationWizard(void) { }
 
@@ -54,6 +54,8 @@ namespace GAME
     // implementation of IButtonMapper
     virtual std::string ControllerID(void) const override { return m_strControllerId; }
     virtual bool NeedsCooldown(void) const override { return true; }
+    virtual bool Emulation(void) const override { return m_bEmulation; }
+    virtual unsigned int ControllerNumber(void) const override { return m_controllerNumber; }
     virtual bool MapPrimitive(JOYSTICK::IButtonMap* buttonMap,
                               JOYSTICK::IActionMap* actionMap,
                               const JOYSTICK::CDriverPrimitive& primitive) override;
@@ -78,6 +80,10 @@ namespace GAME
 
     void OnMotion();
     void OnMotionless();
+
+    // Construction parameters
+    const bool                           m_bEmulation;
+    const unsigned int                   m_controllerNumber;
 
     // Run() parameters
     std::string                          m_strControllerId;
