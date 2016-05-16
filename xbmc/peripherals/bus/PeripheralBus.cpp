2dcd23382a6766f180c788d7115f3ee8cb5cda2e
Garrett Brown <themagnificentmrb@gmail.com>
2016-05-15 19:21:14 -0700

    [PR 9823] [peripherals] Fix skipping initial scan on startup if polling is disabled

--- xbmc/peripherals/bus/PeripheralBus.cpp
+++ xbmc/peripherals/bus/PeripheralBus.cpp
@@ -248,6 +248,11 @@ void CPeripheralBus::Initialise(void)
     Create();
     SetPriority(-1);
   }
+  else
+  {
+    lock.Leave();
+    ScanForDevices();
+  }
 }
 
 void CPeripheralBus::Register(CPeripheral *peripheral)
