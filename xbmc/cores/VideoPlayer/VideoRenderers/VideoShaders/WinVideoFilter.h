517c5d0b38190927f263dc3009f15f7c415905a5
VelocityRa <makren67@gmail.com>
2017-06-16 21:07:58 +0300

    WinRenderer: Nearest neighbor sampling for Pixel Renderer
    * I tried to change as few things as possible by using the
      color shader used in the pixel renderer to do the sampling.
    
    * It's all compile-time and the shader gets recompiled when
      it needs to (if the sampling option changed from/to nearest
      neigbor, to/from something else).

--- xbmc/cores/VideoPlayer/VideoRenderers/VideoShaders/WinVideoFilter.h
+++ xbmc/cores/VideoPlayer/VideoRenderers/VideoShaders/WinVideoFilter.h
@@ -48,6 +48,7 @@ private:
   bool         m_limitedRange;
   EBufferFormat m_format;
   XMFLOAT4X4   m_mat;
+  ESCALINGMETHOD m_scalingMethod;
 };
 
 class CWinShader
@@ -107,7 +108,7 @@ private:
   unsigned m_sourceWidth{ 0 };
   unsigned m_sourceHeight{ 0 };
   CRect m_sourceRect{ 0.f, 0.f, 0.f, 0.f };
-  CPoint m_destPoints[4] = 
+  CPoint m_destPoints[4] =
   {
     { 0.f, 0.f },
     { 0.f, 0.f },
@@ -131,13 +132,14 @@ class CYUV2RGBShader : public CWinShader
 public:
   CYUV2RGBShader();
   virtual ~CYUV2RGBShader();
-  virtual bool Create(EBufferFormat fmt, COutputShader *pOutShader = nullptr);
-  virtual void Render(CRect sourceRect, CPoint dest[], float contrast, float brightness, CRenderBuffer* videoBuffer, CD3DTexture *target);
+  virtual bool Create(EBufferFormat fmt, COutputShader *pOutShader = nullptr, ESCALINGMETHOD scalingMethod = VS_SCALINGMETHOD_LINEAR);
+  virtual void Render(CRect sourceRect, CPoint dest[], float contrast, float brightness, CRenderBuffer* videoBuffer, CD3DTexture *target, ESCALINGMETHOD scalingMethod = VS_SCALINGMETHOD_LINEAR);
 
 protected:
   void PrepareParameters(CRenderBuffer* videoBuffer, CRect sourceRect, CPoint dest[],
                          float contrast, float brightness);
   void SetShaderParameters(CRenderBuffer* videoBuffer);
+  void HandleScalerChange(ESCALINGMETHOD escalingMethod);
 
 private:
   CYUV2RGBMatrix      m_matrix;
@@ -147,6 +149,7 @@ private:
   EBufferFormat       m_format;
   float               m_texSteps[2];
   COutputShader *m_pOutShader;
+  ESCALINGMETHOD m_scalingMethod;
 
   struct CUSTOMVERTEX {
       FLOAT x, y, z;
@@ -191,14 +194,14 @@ public:
   void Render(CD3DTexture &sourceTexture,
               unsigned int sourceWidth, unsigned int sourceHeight,
               unsigned int destWidth, unsigned int destHeight,
-              CRect sourceRect, CRect destRect, bool useLimitRange, 
+              CRect sourceRect, CRect destRect, bool useLimitRange,
               CD3DTexture *target) override;
   CConvolutionShader1Pass() : CConvolutionShader(), m_sourceWidth(0), m_sourceHeight(0) {}
 
 protected:
   void PrepareParameters(unsigned int sourceWidth, unsigned int sourceHeight,
                          CRect sourceRect, CRect destRect);
-  void SetShaderParameters(CD3DTexture &sourceTexture, float* texSteps, 
+  void SetShaderParameters(CD3DTexture &sourceTexture, float* texSteps,
                            int texStepsCount, bool useLimitRange) override;
 
 
@@ -226,7 +229,7 @@ protected:
   void PrepareParameters(unsigned int sourceWidth, unsigned int sourceHeight,
                          unsigned int destWidth, unsigned int destHeight,
                          CRect sourceRect, CRect destRect);
-  void SetShaderParameters(CD3DTexture &sourceTexture, float* texSteps, 
+  void SetShaderParameters(CD3DTexture &sourceTexture, float* texSteps,
                            int texStepsCount, bool useLimitRange) override;
   void SetStepParams(UINT stepIndex) override;
 
