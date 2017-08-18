63368b6049a2dde7891209ad95fec320eef6c3d9
VelocityRa <makren67@gmail.com>
2017-08-19 00:44:01 +0300

    [GSoC 2017] RetroPlayer: Video Shaders
    Includes the following fixes from KOPRajs:
    
      * Calculate separate POT (Power-Of-Two) texture width and height
      * Update video shader preset if size of the source has changed
      * Fix multipass shaders texture scaling and filtering
      * Use optimal FBO texture sizes
      * Clean up multipass rendering logic
      * Set correct destination rectangle size for the last pass

--- xbmc/filesystem/AddonsDirectory.cpp
+++ xbmc/filesystem/AddonsDirectory.cpp
@@ -74,6 +74,7 @@ const std::set<AddonType> lookAndFeelTypes = {
 const std::set<AddonType> gameTypes = {
     AddonType::GAME_CONTROLLER,
     AddonType::GAMEDLL,
+    AddonType::SHADERDLL,
     AddonType::GAME,
     AddonType::RESOURCE_GAMES,
 };
@@ -126,9 +127,15 @@ static bool IsGameResource(const AddonPtr& addon)
 
 static bool IsGameSupportAddon(const AddonPtr& addon)
 {
-  return addon->Type() == AddonType::GAMEDLL &&
+  if (addon->Type() == AddonType::GAMEDLL &&
          !std::static_pointer_cast<GAME::CGameClient>(addon)->SupportsPath() &&
-         !std::static_pointer_cast<GAME::CGameClient>(addon)->SupportsStandalone();
+         !std::static_pointer_cast<GAME::CGameClient>(addon)->SupportsStandalone())
+    return true;
+
+  if (addon->Type() == AddonType::SHADERDLL)
+    return true;
+
+  return false;
 }
 
 static bool IsGameAddon(const AddonPtr& addon)
