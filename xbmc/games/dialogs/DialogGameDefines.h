4ae5827a72cdc3a5d55f7d53aab7a930a928ae5d
VelocityRa <makren67@gmail.com>
2017-08-19 00:44:01 +0300

    [PR 86] RetroPlayer: Video Shaders
    TODO: Fix the following warning, introduced in binary add-ons refactor:
    
    In file included
        from xbmc/addons/ShaderPreset.h:23:0,
        from xbmc/cores/RetroPlayer/shaders/ShaderPresetFactory.cpp:13:
    
    xbmc/addons/kodi-dev-kit/include/kodi/addon-instance/ShaderPreset.h:288:7:
        warning: ‘kodi::addon::CInstanceShaderPreset’ declared with greater
        visibility than the type of its field
        ‘kodi::addon::CInstanceShaderPreset::<anonymous>’ [-Wattributes]
    
     class CInstanceShaderPreset : public IAddonInstance
           ^~~~~~~~~~~~~~~~~~~~~
    
    xbmc/addons/kodi-dev-kit/include/kodi/addon-instance/ShaderPreset.h:288:7:
        warning: ‘kodi::addon::CInstanceShaderPreset’ declared with greater
        visibility than its base ‘kodi::addon::IAddonInstance’ [-Wattributes]

--- xbmc/games/dialogs/DialogGameDefines.h
+++ xbmc/games/dialogs/DialogGameDefines.h
@@ -10,3 +10,6 @@
 
 // Name of list item property for savestate captions
 #define SAVESTATE_CAPTION "savestate.caption"
+
+// String of list item property "game.videofilter" when no filter is set
+#define PROPERTY_NO_VIDEO_FILTER ""
