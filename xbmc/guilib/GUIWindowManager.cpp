3b873a0fc11825e34577ebe84cd888ae9cecf453
Garrett Brown <themagnificentmrb@gmail.com>
2018-06-22 15:50:46 -0700

    Game OSD: Add rewind dialog to root menu (WIP)

--- xbmc/guilib/GUIWindowManager.cpp
+++ xbmc/guilib/GUIWindowManager.cpp
@@ -154,6 +154,7 @@
 #include "games/dialogs/disc/DialogGameDiscManager.h"
 #include "games/dialogs/osd/DialogGameAdvancedSettings.h"
 #include "games/dialogs/osd/DialogGameOSD.h"
+#include "games/dialogs/osd/DialogGameRewind.h"
 #include "games/dialogs/osd/DialogGameSaves.h"
 #include "games/dialogs/osd/DialogGameStretchMode.h"
 #include "games/dialogs/osd/DialogGameVideoFilter.h"
@@ -331,6 +332,7 @@ void CGUIWindowManager::CreateWindows()
   Add(new GAME::CDialogGameVolume);
   Add(new GAME::CDialogGameAdvancedSettings);
   Add(new GAME::CDialogGameVideoRotation);
+  Add(new GAME::CDialogGameRewind);
   Add(new GAME::CDialogInGameSaves);
   Add(new GAME::CDialogSelectAvatar);
   Add(new GAME::CGUIAgentWindow);
@@ -461,6 +463,7 @@ bool CGUIWindowManager::DestroyWindows()
     DestroyWindow(WINDOW_DIALOG_GAME_VOLUME);
     DestroyWindow(WINDOW_DIALOG_GAME_ADVANCED_SETTINGS);
     DestroyWindow(WINDOW_DIALOG_GAME_VIDEO_ROTATION);
+    DestroyWindow(WINDOW_DIALOG_GAME_REWIND);
     DestroyWindow(WINDOW_DIALOG_IN_GAME_SAVES);
     DestroyWindow(WINDOW_DIALOG_GAME_AGENTS);
     DestroyWindow(WINDOW_DIALOG_GAME_DISC_MANAGER);
