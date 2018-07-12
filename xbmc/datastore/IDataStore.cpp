dab56b085781456f0aeb5ffb92c7b628b4d69516
Garrett Brown <themagnificentmrb@gmail.com>
2018-07-12 14:31:05 -0700

    Filesystem: Add IPFS implementation
    This adds an abstraction for a data store. LMDB is included as an
    implementation.
    
    A block store is built on the data store abstraction. Blocks are chunks
    data addressed by a CID (content identifier).

---
+++ xbmc/datastore/IDataStore.cpp
@@ -0,0 +1,14 @@
+/*
+ *  Copyright (C) 2022 Team Kodi
+ *  This file is part of Kodi - https://kodi.tv
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *  See LICENSES/README.md for more information.
+ */
+
+#include "IDataStore.h"
+
+using namespace KODI;
+using namespace DATASTORE;
+
+IDataStore::~IDataStore() = default;
