48a5af3e4494cbca4ac5b60b3b1ff2b3c3e0c328
Kostas Andrianos <gusandrianos@gmail.com>
2019-08-07 14:36:08 +0300

    [GSoC 2019] RetroPlayer: OpenGL/GLES/DMA back-end for shaders
    OpenGL
    ======
    
    Includes the following fixes from KOPRajs:
    
      * Remove renderer code not needed for video shaders from common path
      * Unbind GL_ARRAY_BUFFER to fix GUI texture corruption in GLES
      * Fix lookup textures (LUTs) support
      * Fix the CShaderGL::PrepareParameters() logic to prevent overwriting
      * Update video shader preset if size of the source has changed
      * Fix multipass shaders texture scaling and filtering
      * Use optimal FBO texture sizes
      * Clean up multipass rendering logic
      * Set correct destination rectangle size for the last pass
    
    OpenGLES
    ========
    
    Includes the following fixes from KOPRajs:
    
      * Update video shader preset if size of the source has changed
      * Fix multipass shaders texture scaling and filtering
    
    DMA
    ===
    
    Includes the following fixes from KOPRajs:
    
      * Update video shader preset if size of the source has changed
      * Fix multipass shaders texture scaling and filtering
    
    General cleanup/fixes
    =====================
    
    Includes the following general cleanup/fixes from KOPRajs:
    
      * Free GL resources
      * Fix GL framebuffer mipmapping support
      * Implement GLSL frame uniforms
      * Remove DX Sampler from IShader
      * Remove DX specific code from common interface
      * Sync SetShaderParameters()
      * Unbind GL program and fix GL sRGB framebuffer support
      * Use GL triangle strip topology
      * Sync GL buffers order
      * Fix GLSL header and version
      * Clean up LUTs
      * Clean up and sync DX and GL shader code
      * Do not create texture for the last pass
      * Fix zero FBO scale
      * Disable usage of optimal texture sizes
      * Add StripParameterPragmas() and remove unspecified scale check for the last pass
    
    And finally the changes:
    
      * Sync GLES code
      * Modernize code style for DX shaders
      * Modernize code style for GL/GLES shaders
    
    And then some more fixes from KOPRajs:
    
      * Fix shaders for macOS
      * Fix copy-paste re-styling error

---
+++ xbmc/cores/RetroPlayer/shaders/gl/ShaderPresetGL.cpp
@@ -0,0 +1,442 @@
+/*
+ *  Copyright (C) 2019-2025 Team Kodi
+ *  This file is part of Kodi - https://kodi.tv
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *  See LICENSES/README.md for more information.
+ */
+
+#include "ShaderPresetGL.h"
+
+#include "ServiceBroker.h"
+#include "ShaderGL.h"
+#include "ShaderLutGL.h"
+#include "ShaderUtilsGL.h"
+#include "cores/RetroPlayer/rendering/RenderContext.h"
+#include "cores/RetroPlayer/shaders/ShaderPresetFactory.h"
+#include "cores/RetroPlayer/shaders/ShaderUtils.h"
+#include "rendering/gl/RenderSystemGL.h"
+#include "utils/log.h"
+
+#include <regex>
+
+using namespace KODI;
+using namespace SHADER;
+
+CShaderPresetGL::CShaderPresetGL(RETRO::CRenderContext& context,
+                                 unsigned int videoWidth,
+                                 unsigned int videoHeight)
+  : m_context(context), m_videoSize(videoWidth, videoHeight)
+{
+  CRect viewPort;
+  m_context.GetViewPort(viewPort);
+  m_outputSize = {viewPort.Width(), viewPort.Height()};
+}
+
+CShaderPresetGL::~CShaderPresetGL()
+{
+  DisposeShaders();
+
+  // The GUI is going to render after this, so apply the state required
+  m_context.ApplyStateBlock();
+}
+
+bool CShaderPresetGL::ReadPresetFile(const std::string& presetPath)
+{
+  return CServiceBroker::GetGameServices().VideoShaders().LoadPreset(presetPath, *this);
+}
+
+bool CShaderPresetGL::RenderUpdate(const CPoint dest[],
+                                   IShaderTexture* source,
+                                   IShaderTexture* target)
+{
+  // Save the viewport
+  CRect viewPort;
+  m_context.GetViewPort(viewPort);
+
+  // Handle resizing of the viewport (window)
+  UpdateViewPort(viewPort);
+
+  // Update shaders/shader textures if required
+  if (!Update())
+    return false;
+
+  PrepareParameters(dest, source, target);
+
+  const unsigned int numPasses = static_cast<unsigned int>(m_pShaders.size());
+
+  // Apply all passes except the last one (which needs to be applied to the backbuffer)
+  for (unsigned int shaderIdx = 0; shaderIdx + 1 < numPasses; ++shaderIdx)
+  {
+    IShader* shader = m_pShaders[shaderIdx].get();
+    IShaderTexture* texture = m_pShaderTextures[shaderIdx].get();
+    RenderShader(shader, source, texture);
+    source = texture;
+  }
+
+  // Restore our viewport
+  m_context.SetViewPort(viewPort);
+  m_context.SetScissors(viewPort);
+
+  // Apply the last pass and write to target (backbuffer) instead of the last texture
+  IShader* lastShader = m_pShaders.back().get();
+  lastShader->Render(source, target);
+
+  m_frameCount += static_cast<float>(m_speed);
+  return true;
+}
+
+void CShaderPresetGL::SetVideoSize(unsigned int videoWidth, unsigned int videoHeight)
+{
+  if (videoWidth != static_cast<unsigned int>(m_videoSize.x) ||
+      videoHeight != static_cast<unsigned int>(m_videoSize.y))
+  {
+    m_videoSize = {videoWidth, videoHeight};
+    m_bPresetNeedsUpdate = true;
+  }
+}
+
+bool CShaderPresetGL::SetShaderPreset(const std::string& shaderPresetPath)
+{
+  m_bPresetNeedsUpdate = true;
+  m_presetPath = shaderPresetPath;
+  return Update();
+}
+
+const std::string& CShaderPresetGL::GetShaderPreset() const
+{
+  return m_presetPath;
+}
+
+bool CShaderPresetGL::Update()
+{
+  auto updateFailed = [this](const std::string& msg)
+  {
+    m_failedPaths.insert(m_presetPath);
+    CLog::Log(LOGWARNING, "CShaderPresetGL::Update: {}", msg);
+    DisposeShaders();
+    return false;
+  };
+
+  if (m_bPresetNeedsUpdate && !HasPathFailed(m_presetPath))
+  {
+    DisposeShaders();
+
+    if (m_presetPath.empty())
+      // No preset should load, just return false, we shouldn't add "" to the failed paths
+      return false;
+
+    if (!ReadPresetFile(m_presetPath))
+    {
+      CLog::LogF(LOGERROR, "Couldn't load shader preset {} or the shaders it references",
+                 m_presetPath);
+      return false;
+    }
+
+    if (!CreateShaders())
+      return updateFailed("Failed to initialize shaders");
+
+    if (!CreateShaderTextures())
+      return updateFailed("A shader texture failed to init");
+  }
+
+  if (m_pShaders.empty())
+    return false;
+
+  // Each pass except the last one must have its own texture and the opposite is also true
+  if (m_pShaders.size() != m_pShaderTextures.size() + 1)
+    return updateFailed("A shader or texture failed to init");
+
+  m_bPresetNeedsUpdate = false;
+  return true;
+}
+
+void CShaderPresetGL::UpdateViewPort()
+{
+  CRect viewPort;
+  m_context.GetViewPort(viewPort);
+  UpdateViewPort(viewPort);
+}
+
+void CShaderPresetGL::UpdateViewPort(CRect viewPort)
+{
+  float2 currentViewPortSize = {viewPort.Width(), viewPort.Height()};
+  if (currentViewPortSize != m_outputSize)
+  {
+    m_outputSize = currentViewPortSize;
+    m_bPresetNeedsUpdate = true;
+    Update();
+  }
+}
+
+void CShaderPresetGL::UpdateMVPs()
+{
+  for (auto& videoShader : m_pShaders)
+    videoShader->UpdateMVP();
+}
+
+void CShaderPresetGL::PrepareParameters(const CPoint dest[],
+                                        IShaderTexture* source,
+                                        IShaderTexture* target)
+{
+  if (m_dest[0] != dest[0] || m_dest[1] != dest[1] || m_dest[2] != dest[2] ||
+      m_dest[3] != dest[3] || target->GetWidth() != m_outputSize.x ||
+      target->GetHeight() != m_outputSize.y)
+  {
+    for (size_t i = 0; i < 4; ++i)
+      m_dest[i] = dest[i];
+
+    m_outputSize = {target->GetWidth(), target->GetHeight()};
+
+    // Update projection matrix and update video shaders
+    UpdateMVPs();
+    UpdateViewPort();
+  }
+
+  const unsigned int numPasses = static_cast<unsigned int>(m_pShaders.size());
+
+  // Prepare parameters for all shader passes
+  for (unsigned int shaderIdx = 0; shaderIdx < numPasses; ++shaderIdx)
+  {
+    auto& videoShader = m_pShaders[shaderIdx];
+    videoShader->PrepareParameters(m_dest, source, m_pShaderTextures, m_pShaders,
+                                   static_cast<uint64_t>(m_frameCount));
+  }
+}
+
+bool CShaderPresetGL::CreateShaders()
+{
+  const unsigned int numPasses = static_cast<unsigned int>(m_passes.size());
+
+  //! @todo Is this pass specific?
+  std::vector<std::shared_ptr<IShaderLut>> passLUTsGL;
+  for (unsigned int shaderIdx = 0; shaderIdx < numPasses; ++shaderIdx)
+  {
+    const ShaderPass& pass = m_passes[shaderIdx];
+    const unsigned int numPassLuts = static_cast<unsigned int>(pass.luts.size());
+
+    for (unsigned int i = 0; i < numPassLuts; ++i)
+    {
+      const ShaderLut& lutStruct = pass.luts[i];
+
+      std::shared_ptr<CShaderLutGL> passLut =
+          std::make_shared<CShaderLutGL>(lutStruct.strId, lutStruct.path);
+      if (passLut->Create(m_context, lutStruct))
+        passLUTsGL.emplace_back(std::move(passLut));
+    }
+
+    // Create the shader
+    std::unique_ptr<CShaderGL> videoShader = std::make_unique<CShaderGL>(m_context);
+
+    const std::string& shaderSource = pass.vertexSource; // Also contains fragment source
+    const std::string& shaderPath = pass.sourcePath;
+
+    // Get only the parameters belonging to this specific shader
+    ShaderParameterMap passParameters = GetShaderParameters(pass.parameters, pass.vertexSource);
+
+    if (!videoShader->Create(shaderSource, shaderPath, passParameters, passLUTsGL, m_outputSize,
+                             shaderIdx, pass.frameCountMod))
+    {
+      CLog::Log(LOGERROR, "Couldn't create a video shader");
+      return false;
+    }
+    m_pShaders.push_back(std::move(videoShader));
+  }
+
+  return true;
+}
+
+bool CShaderPresetGL::CreateShaderTextures()
+{
+  m_pShaderTextures.clear();
+
+  unsigned int major, minor;
+  CServiceBroker::GetRenderSystem()->GetRenderVersion(major, minor);
+
+  float2 prevSize = m_videoSize;
+  float2 prevTextureSize = m_videoSize;
+
+  const unsigned int numPasses = static_cast<unsigned int>(m_passes.size());
+
+  for (unsigned int shaderIdx = 0; shaderIdx < numPasses; ++shaderIdx)
+  {
+    const auto& pass = m_passes[shaderIdx];
+
+    // Resolve final texture resolution, taking scale type and scale multiplier into account
+    float2 scaledSize, textureSize;
+    switch (pass.fbo.scaleX.scaleType)
+    {
+      case ScaleType::ABSOLUTE_SCALE:
+        scaledSize.x = static_cast<float>(pass.fbo.scaleX.abs);
+        break;
+      case ScaleType::VIEWPORT:
+        scaledSize.x =
+            pass.fbo.scaleX.scale ? pass.fbo.scaleX.scale * m_outputSize.x : m_outputSize.x;
+        break;
+      case ScaleType::INPUT:
+      default:
+        scaledSize.x = pass.fbo.scaleX.scale ? pass.fbo.scaleX.scale * prevSize.x : prevSize.x;
+        break;
+    }
+    switch (pass.fbo.scaleY.scaleType)
+    {
+      case ScaleType::ABSOLUTE_SCALE:
+        scaledSize.y = static_cast<float>(pass.fbo.scaleY.abs);
+        break;
+      case ScaleType::VIEWPORT:
+        scaledSize.y =
+            pass.fbo.scaleY.scale ? pass.fbo.scaleY.scale * m_outputSize.y : m_outputSize.y;
+        break;
+      case ScaleType::INPUT:
+      default:
+        scaledSize.y = pass.fbo.scaleY.scale ? pass.fbo.scaleY.scale * prevSize.y : prevSize.y;
+        break;
+    }
+
+    if (shaderIdx + 1 == numPasses)
+    {
+      // We're supposed to output at full (viewport) resolution
+      scaledSize.x = m_outputSize.x;
+      scaledSize.y = m_outputSize.y;
+    }
+    else
+    {
+      // Determine the framebuffer data format
+      GLint internalFormat;
+      GLenum pixelFormat;
+      if (pass.fbo.floatFramebuffer && major >= 3)
+      {
+        // Give priority to float framebuffer parameter (we can't use both float and sRGB)
+        internalFormat = GL_RGBA32F;
+        pixelFormat = GL_RGBA;
+      }
+      else
+      {
+        if (pass.fbo.sRgbFramebuffer && major >= 3)
+        {
+          internalFormat = GL_SRGB8_ALPHA8;
+          pixelFormat = GL_RGBA;
+        }
+        else
+        {
+          internalFormat = GL_RGBA8;
+          pixelFormat = GL_BGRA;
+        }
+      }
+
+      //! @todo Enable usage of optimal texture sizes when all issues are fixed
+      textureSize = scaledSize; // CShaderUtils::GetOptimalTextureSize(scaledSize)
+
+      CGLTexture* textureGL = new CGLTexture(static_cast<unsigned int>(textureSize.x),
+                                             static_cast<unsigned int>(textureSize.y),
+                                             XB_FMT_A8R8G8B8); // Format is not used
+
+      textureGL->CreateTextureObject();
+
+      if (textureGL->getMTexture() <= 0)
+      {
+        CLog::Log(LOGERROR, "Couldn't create a texture for video shader {}.", pass.sourcePath);
+        return false;
+      }
+
+      ShaderPass& nextPass = m_passes[shaderIdx + 1];
+
+      const GLint wrapType = CShaderUtilsGL::TranslateWrapType(nextPass.wrapType);
+      const GLuint magFilterType =
+          (nextPass.filterType == FilterType::LINEAR ? GL_LINEAR : GL_NEAREST);
+      const GLuint minFilterType =
+          (nextPass.mipmap ? (nextPass.filterType == FilterType::LINEAR ? GL_LINEAR_MIPMAP_LINEAR
+                                                                        : GL_NEAREST_MIPMAP_NEAREST)
+                           : magFilterType);
+
+      glBindTexture(GL_TEXTURE_2D, textureGL->getMTexture());
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, magFilterType);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, minFilterType);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, wrapType);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, wrapType);
+      glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, textureSize.x, textureSize.y, 0, pixelFormat,
+                   internalFormat == GL_RGBA32F ? GL_FLOAT : GL_UNSIGNED_BYTE, (void*)0);
+
+      GLfloat blackBorder[4] = {0.0f, 0.0f, 0.0f, 0.0f};
+      glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, blackBorder);
+
+      m_pShaderTextures.emplace_back(
+          new CShaderTextureGL(*textureGL, nextPass.mipmap, pass.fbo.sRgbFramebuffer));
+    }
+
+    // Notify shader of its source and dest size
+    m_pShaders[shaderIdx]->SetSizes(prevSize, prevTextureSize, scaledSize);
+
+    prevSize = scaledSize;
+    prevTextureSize = textureSize;
+  }
+
+  UpdateMVPs();
+  return true;
+}
+
+void CShaderPresetGL::RenderShader(IShader* shader,
+                                   IShaderTexture* source,
+                                   IShaderTexture* target) const
+{
+  if (static_cast<CShaderTextureGL*>(target)->BindFBO())
+  {
+    const CRect newViewPort(0.f, 0.f, target->GetWidth(), target->GetHeight());
+    glViewport((GLsizei)newViewPort.x1, (GLsizei)newViewPort.y1, (GLsizei)newViewPort.x2,
+               (GLsizei)newViewPort.y2);
+    glScissor((GLsizei)newViewPort.x1, (GLsizei)newViewPort.y1, (GLsizei)newViewPort.x2,
+              (GLsizei)newViewPort.y2);
+
+    shader->Render(source, target);
+    static_cast<CShaderTextureGL*>(target)->UnbindFBO();
+  }
+}
+
+void CShaderPresetGL::DisposeShaders()
+{
+  m_pShaders.clear();
+  m_pShaderTextures.clear();
+  m_passes.clear();
+  m_bPresetNeedsUpdate = true;
+}
+
+bool CShaderPresetGL::HasPathFailed(const std::string& path) const
+{
+  return m_failedPaths.find(path) != m_failedPaths.end();
+}
+
+ShaderParameterMap CShaderPresetGL::GetShaderParameters(
+    const std::vector<ShaderParameter>& parameters, const std::string& sourceStr) const
+{
+  static const std::regex pragmaParamRegex("#pragma parameter ([a-zA-Z_][a-zA-Z0-9_]*)");
+  std::smatch matches;
+
+  std::vector<std::string> validParams;
+  std::string::const_iterator searchStart(sourceStr.cbegin());
+  while (regex_search(searchStart, sourceStr.cend(), matches, pragmaParamRegex))
+  {
+    validParams.push_back(matches[1].str());
+    searchStart += matches.position() + matches.length();
+  }
+
+  ShaderParameterMap matchParams;
+
+  // For each param found in the source code
+  for (const auto& match : validParams)
+  {
+    // For each param found in the preset file
+    for (const ShaderParameter& parameter : parameters)
+    {
+      // Check if they match
+      if (match == parameter.strId)
+      {
+        // The add-on has already handled parsing and overwriting default
+        // parameter values from the preset file. The final value we
+        // should use is in the 'current' field.
+        matchParams[match] = parameter.current;
+        break;
+      }
+    }
+  }
+  return matchParams;
+}
