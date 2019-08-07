e88c67d326b0ce09de2500b14e2a77403cac945a
Kostas Andrianos <gusandrianos@gmail.com>
2019-08-07 14:36:08 +0300

    [GSoC 2019] RetroPlayer: OpenGL/GLES/DMA back-end for shaders
    OpenGL
    ======
    
    Includes the following fixes from KOPRajs:
    
      * Remove renderer code not needed for video shaders from common path
      * Unbind GL_ARRAY_BUFFER to fix GUI texture corruption in GLES
      * Fix lookup textures (LUTs) support
      * Fix the CShaderGL::PrepareParameters() logic to prevent overwriting
      * Update video shader preset if size of the source has changed
      * Fix multipass shaders texture scaling and filtering
      * Use optimal FBO texture sizes
      * Clean up multipass rendering logic
      * Set correct destination rectangle size for the last pass
    
    OpenGLES
    ========
    
    Includes the following fixes from KOPRajs:
    
      * Update video shader preset if size of the source has changed
      * Fix multipass shaders texture scaling and filtering
    
    DMA
    ===
    
    Includes the following fixes from KOPRajs:
    
      * Update video shader preset if size of the source has changed
      * Fix multipass shaders texture scaling and filtering
    
    General cleanup/fixes
    =====================
    
    Includes the following general cleanup/fixes from KOPRajs:
    
      * Free GL resources
      * Fix GL framebuffer mipmapping support
      * Implement GLSL frame uniforms
      * Remove DX Sampler from IShader
      * Remove DX specific code from common interface
      * Sync SetShaderParameters()
      * Unbind GL program and fix GL sRGB framebuffer support
      * Use GL triangle strip topology
      * Sync GL buffers order
      * Fix GLSL header and version
      * Clean up LUTs
      * Clean up and sync DX and GL shader code
      * Do not create texture for the last pass
      * Fix zero FBO scale
      * Disable usage of optimal texture sizes
      * Add StripParameterPragmas() and remove unspecified scale check for the last pass
    
    And finally the changes:
    
      * Sync GLES code
      * Modernize code style for DX shaders
      * Modernize code style for GL/GLES shaders
    
    And then some more fixes from KOPRajs:
    
      * Fix shaders for macOS
      * Fix copy-paste re-styling error

---
+++ xbmc/cores/RetroPlayer/shaders/gl/ShaderTextureGL.h
@@ -0,0 +1,54 @@
+/*
+ *  Copyright (C) 2019-2025 Team Kodi
+ *  This file is part of Kodi - https://kodi.tv
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *  See LICENSES/README.md for more information.
+ */
+
+#pragma once
+
+#include "cores/RetroPlayer/shaders/IShaderTexture.h"
+#include "guilib/Texture.h"
+#include "guilib/TextureGL.h"
+
+namespace KODI
+{
+namespace SHADER
+{
+
+class CShaderTextureGL : public IShaderTexture
+{
+public:
+  CShaderTextureGL() = default;
+  CShaderTextureGL(CGLTexture* texture, bool mipmap = 0, bool sRgbFramebuffer = 0)
+    : m_texture(texture), m_mipmap(mipmap), m_sRgbFramebuffer(sRgbFramebuffer)
+  {
+  }
+  CShaderTextureGL(CGLTexture& texture, bool mipmap = 0, bool sRgbFramebuffer = 0)
+    : m_texture(&texture), m_mipmap(mipmap), m_sRgbFramebuffer(sRgbFramebuffer)
+  {
+  }
+
+  // Destructor
+  ~CShaderTextureGL() override;
+
+  float GetWidth() const override { return static_cast<float>(m_texture->GetWidth()); }
+  float GetHeight() const override { return static_cast<float>(m_texture->GetHeight()); }
+
+  CGLTexture* GetPointer() { return m_texture; }
+  bool IsMipmapped() { return m_mipmap; }
+  bool IsSRGBFramebuffer() { return m_sRgbFramebuffer; }
+
+  bool CreateFBO();
+  bool BindFBO();
+  void UnbindFBO();
+
+private:
+  CGLTexture* m_texture{nullptr};
+  bool m_mipmap{0};
+  bool m_sRgbFramebuffer{0};
+  GLuint FBO{0};
+};
+} // namespace SHADER
+} // namespace KODI
