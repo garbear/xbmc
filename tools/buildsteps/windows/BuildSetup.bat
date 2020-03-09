66361d5571470dcce4716d41c6be7a19113cd0b0
Garrett Brown <themagnificentmrb@gmail.com>
2020-03-09 00:17:38 -0700

    [temp] Package binary add-ons:
      * game.libretro
      * game.shader.presets
      * peripheral.joystick
      * vfs.libarchive
      * vfs.rar

--- tools/buildsteps/windows/BuildSetup.bat
+++ tools/buildsteps/windows/BuildSetup.bat
@@ -160,7 +160,11 @@ set WORKSPACE=%base_dir%\kodi-build.%TARGET_PLATFORM%
     IF %buildmode%==clean (
       call make-addons.bat clean
     )
-    call make-addons.bat
+    CALL make-addons.bat ^game\.libretro$
+    CALL make-addons.bat ^game\.shader\.presets$
+    CALL make-addons.bat ^peripheral\.joystick$
+    CALL make-addons.bat ^vfs\.libarchive$
+    CALL make-addons.bat ^vfs\.rar$
     IF %errorlevel%==1 (
       set DIETEXT="failed to build addons"
       cd %build_path%
