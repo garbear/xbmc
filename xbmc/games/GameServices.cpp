a55941e9288c739a4d9ecc49cb388cf98709b879
Garrett Brown <themagnificentmrb@gmail.com>
2020-06-07 14:24:29 -0700

    Testing crypto and DID

--- xbmc/games/GameServices.cpp
+++ xbmc/games/GameServices.cpp
@@ -14,6 +14,14 @@
 #include "games/GameSettings.h"
 #include "profiles/ProfileManager.h"
 
+// Crypto testing
+#include "crypto/CryptoProvider.h"
+#include "crypto/CryptoTypes.h"
+#include "crypto/Key.h"
+#include "crypto/codecs/Base58.h"
+#include "crypto/ed25519/OpenSSLEd25519Provider.h"
+#include "crypto/random/OpenSSLRandomGenerator.h"
+
 using namespace KODI;
 using namespace GAME;
 
@@ -28,6 +36,79 @@ CGameServices::CGameServices(CControllerManager& controllerManager,
     m_gameSettings(new CGameSettings()),
     m_videoShaders(new SHADER::CShaderPresetFactory(addons))
 {
+  // Create CSPRNG
+  std::unique_ptr<CRYPTO::IRandomGenerator> csprng(new CRYPTO::COpenSSLRandomGenerator);
+  CRYPTO::ByteArray buffer = csprng->RandomBytes(32);
+
+  // Create Ed25519 signature scheme
+  std::unique_ptr<CRYPTO::IEd25519Provider> ed25519Provider(new CRYPTO::COpenSSLEd25519Provider);
+  std::unique_ptr<CRYPTO::IRandomGenerator> csrng(new CRYPTO::COpenSSLRandomGenerator);
+
+  // Create crypto provider
+  CRYPTO::CCryptoProvider cryptoProvider(std::move(ed25519Provider), std::move(csrng));
+
+  // Generate public/private key pair
+  auto keyPair = cryptoProvider.GenerateKeys(CRYPTO::Key::Type::Ed25519);
+
+  /*!
+   * DID format:
+   *
+   *   {
+   *     "@context": "https://www.w3.org/ns/did/v1",
+   *     "id": "did:example:123456789abcdefghi",
+   *     "authentication": [
+   *       {
+   *         "id": "did:example:123456789abcdefghi#keys-1",
+   *         "type": "Ed25519VerificationKey2018",
+   *         "controller": "did:example:123",
+   *         "publicKeyBase58": "H3C2AVvLMv6gmMNam3uVAjZpfkcJCwDwnZn6z3wXmqPV"
+   *       }
+   *     ],
+   *     "service": [{
+   *       "id":"did:example:123456789abcdefghi#vcs",
+   *       "type": "VerifiableCredentialService",
+   *       "serviceEndpoint": "https://example.com/vc/"
+   *     }]
+   *   }
+   */
+  const std::string id = "did:example:123456789abcdefghi"; //! @todo
+  const std::string type = "Ed25519VerificationKey2018"; //! @todo
+  const std::string controller = "did:example:123"; //! @todo
+  auto publicKeyBase58 = CRYPTO::CBase58::EncodeBase58(keyPair.publicKey.data);
+
+  // Inter-Planetary Naming System (IPNS)
+  //
+  // Create some content to publish
+  //
+  // DID syntax
+  //
+  //   Scheme: did
+  //   Method: ipid
+  //   IPFS ID: QmeJGfbW6bhapSfyjV5kDq5wt3h2g46Pwj15pJBVvy7jM3
+  //
+  std::string content = "did:ipid:QmeJGfbW6bhapSfyjV5kDq5wt3h2g46Pwj15pJBVvy7jM3";
+
+  // Sample DDO stored using did method spec stored on IPFS
+  //
+  //   {
+  //     "@context": "/ipfs/QmfS56jDfrXNaS6Xcsp3RJiXd2wyY7smeEAwyTAnL1RhEG",
+  //     "id": "did:ipid:<IPFS ID>",
+  //     "owner": [{
+  //       "id": "did:ipid:<IPFS ID>",
+  //       "type": ["CryptographicKey", "EdDsaPublicKey"],
+  //       "curve": "ed25519",
+  //       "expires": "2100-01-01T00:00:00Z",
+  //       "publicKeyBase64": "lji9qTtkCydxtez/bt1zdLxVMMbz4SzWvlqgOBmURoM="
+  //     }, {
+  //       "id": "did:ipid:<IPFS ID>",
+  //       "type": ["CryptographicKey", "Ed25519VerificationKey2018"],
+  //       "expires": "2100-01-01T00:00:00Z",
+  //       "publicKeyBase58": "H3C2AVvLMv6gmMNam3uVAjZpfkcJCwDwnZn6z3wXmqPV"
+  //     }],
+  //     "created": "2017-09-24T17:00:00Z",
+  //     "updated": "2018-09-24T02:41:00Z",
+  //     "signature": {} //! @todo
+  //   }
 }
 
 CGameServices::~CGameServices() = default;
