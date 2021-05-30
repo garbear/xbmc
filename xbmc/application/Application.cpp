7e62dd9a9f1f3c2791e23173cc2585965b8026cf
Garrett Brown <themagnificentmrb@gmail.com>
2021-05-30 02:53:08 -0700

    [PR 21183] Smart Home: Camera and input streaming

--- xbmc/application/Application.cpp
+++ xbmc/application/Application.cpp
@@ -122,6 +122,7 @@
 #include "settings/MediaSettings.h"
 #include "settings/Settings.h"
 #include "settings/SettingsComponent.h"
+#include "smarthome/SmartHomeServices.h"
 #include "speech/ISpeechRecognition.h"
 #include "storage/MediaManager.h"
 #include "threads/SingleLock.h"
@@ -1888,6 +1889,8 @@ void CApplication::FrameMove(bool processEvents, bool processGUI)
   }
 
   appPlayer->FrameMove();
+
+  CServiceBroker::GetSmartHomeServices().FrameMove();
 
   // this will go away when render systems gets its own thread
   CServiceBroker::GetWinSystem()->DriveRenderLoop();
