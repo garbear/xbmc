cd3faabcc78822ee80cb4a3c6d1e0e0b547e95eb
Garrett Brown <themagnificentmrb@gmail.com>
2021-06-04 20:43:24 -0700

    [temp] Wayland: Scale width by 2 for dual monitor span

--- xbmc/windowing/wayland/WindowDecorator.cpp
+++ xbmc/windowing/wayland/WindowDecorator.cpp
@@ -683,6 +683,9 @@ bool CWindowDecorator::StateHasWindowDecorations(IShellSurface::StateBitset stat
 
 CSizeInt CWindowDecorator::CalculateMainSurfaceSize(CSizeInt size, IShellSurface::StateBitset state) const
 {
+  //! @todo Dual monitor span
+  size.SetWidth(size.Width() * 2);
+
   if (StateHasWindowDecorations(state))
   {
     // Subtract decorations
@@ -697,6 +700,9 @@ CSizeInt CWindowDecorator::CalculateMainSurfaceSize(CSizeInt size, IShellSurface
 
 CSizeInt CWindowDecorator::CalculateFullSurfaceSize(CSizeInt size, IShellSurface::StateBitset state) const
 {
+  //! @todo Dual monitor span
+  size.SetWidth(size.Width() * 2);
+
   if (StateHasWindowDecorations(state))
   {
     // Add decorations
