214280bb9dfcd104be42f295d9b2ece8d63b08a9
Garrett Brown <themagnificentmrb@gmail.com>
2021-08-27 16:28:34 -0700

    Revert "[PR 114] RetroPlayer: OpenGL back-end for shaders"
    This reverts commit 6a09d17585937be80c562b830cb96404666abeba.

--- xbmc/games/dialogs/osd/DialogGameVideoFilter.cpp
+++ xbmc/games/dialogs/osd/DialogGameVideoFilter.cpp
@@ -101,16 +101,9 @@ void CDialogGameVideoFilter::InitVideoFilters()
 {
   std::vector<VideoFilterProperties> videoFilters;
 
-  std::string xmlFilename;
-#ifdef TARGET_WINDOWS
-  xmlFilename = "ShaderPresetsHLSLP.xml";
-#else
-  xmlFilename = "ShaderPresetsGLSLP.xml";
-#endif
-
   // TODO: Have the add-on give us the xml as a string (or parse it)
   static const std::string addonPath = std::string("special://xbmcbinaddons/") + PRESETS_ADDON_NAME;
-  static const std::string xmlPath = addonPath + "/resources/" + xmlFilename;
+  static const std::string xmlPath = "special://xbmc/system/shaders/presets/shader-manifest.xml";
   std::string basePath = URIUtils::GetBasePath(xmlPath);
 
   CXBMCTinyXML xml = CXBMCTinyXML(xmlPath);
