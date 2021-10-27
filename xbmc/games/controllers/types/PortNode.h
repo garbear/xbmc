4132c0824a91ba75d0edb0bff6a8dfee8b90368a
Garrett Brown <themagnificentmrb@gmail.com>
2021-10-27 02:59:06 -0700

    [PR 20408] Game API v2.2.0: Add option to prevent disconnecting controllers
    Needed for Multitap support for Snes9x cores.

--- xbmc/games/controllers/types/PortNode.h
+++ xbmc/games/controllers/types/PortNode.h
@@ -73,6 +73,13 @@ public:
   void SetAddress(std::string address);
 
   /*!
+   * \brief If true, prevents a disconnection option from being shown for this
+   * port
+   */
+  bool IsForceConnected() const { return m_forceConnected; }
+  void SetForceConnected(bool forceConnected) { m_forceConnected = forceConnected; }
+
+  /*!
    * \brief Return the controller profiles that are compatible with this port
    *
    * \return The controller profiles, or empty if this port doesn't support
@@ -108,6 +115,7 @@ private:
   PORT_TYPE m_portType = PORT_TYPE::UNKNOWN;
   std::string m_portId;
   std::string m_address;
+  bool m_forceConnected{true};
   ControllerNodeVec m_controllers;
 };
 
