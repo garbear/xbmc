f229161081baaba80931104b1e9fb836894751d5
Garrett Brown <themagnificentmrb@gmail.com>
2021-10-29 09:08:14 -0700

    Game add-ons: Fix silent error when hardware rendering fails
    After fixing this and trying to load a PNG in 3DEngine, the following
    error is logged:
    
      ERROR <general>: GAME: No stream implementation for type: 3

--- xbmc/games/addons/GameClient.cpp
+++ xbmc/games/addons/GameClient.cpp
@@ -230,6 +230,9 @@ bool CGameClient::OpenFile(const CFileItem& file,
 
   GAME_ERROR error = GAME_ERROR_FAILED;
 
+  // Loading the game might require the stream subsystem to be initialized
+  Streams().Initialize(streamManager);
+
   try
   {
     LogError(error = m_struct.toAddon->LoadGame(&m_struct, path.c_str()), "LoadGame()");
@@ -242,11 +245,13 @@ bool CGameClient::OpenFile(const CFileItem& file,
   if (error != GAME_ERROR_NO_ERROR)
   {
     NotifyError(error);
+    Streams().Deinitialize();
     return false;
   }
 
   if (!InitializeGameplay(file.GetPath(), streamManager, input))
   {
+    Streams().Deinitialize();
     return false;
   }
 
@@ -295,7 +300,6 @@ bool CGameClient::InitializeGameplay(const std::string& gamePath,
 {
   if (LoadGameInfo())
   {
-    Streams().Initialize(streamManager);
     Input().Start(input);
 
     m_bIsPlaying = true;
