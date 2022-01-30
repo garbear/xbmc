b9fdeb23593541294b7ac32cd20835683300a46b
Garrett Brown <themagnificentmrb@gmail.com>
2022-01-29 18:59:01 -0800

    RetroPlayer: Improve documentation

--- xbmc/cores/RetroPlayer/guibridge/GUIGameSettingsHandle.h
+++ xbmc/cores/RetroPlayer/guibridge/GUIGameSettingsHandle.h
@@ -23,10 +23,45 @@ public:
   CGUIGameSettingsHandle(CGUIGameRenderManager& renderManager);
   virtual ~CGUIGameSettingsHandle();
 
+  /*!
+   * \brief Get the ID of the active game client
+   *
+   * \return The ID of the active game client, or empty string if a game is
+   * not playing
+   */
   std::string GameClientID();
+
+  /*!
+   * \brief Get the full path of the game being played
+   *
+   * \return The game's path, or empty string if a game is not playing
+   */
   std::string GetPlayingGame();
+
+  /*!
+   * \brief Create a savestate of the current game being played
+   *
+   * \param autosave True if the save was invoked automatically, or false if
+   * the save was invoked by a player
+   *
+   * \return The path to the created savestate file, or empty string on
+   * failure or if a game is not playing
+   */
   std::string CreateSavestate(bool autosave);
+
+  /*!
+   * \brief Load a savestate for the current game being played
+   *
+   * \param path The path to the created savestate file returned by
+   * CreateSavestate()
+   *
+   * \return True if the savestate was loaded successfully, false otherwise
+   */
   bool LoadSavestate(const std::string& path);
+
+  /*!
+   * \brief Close the in-game OSD
+   */
   void CloseOSD();
 
 private:
