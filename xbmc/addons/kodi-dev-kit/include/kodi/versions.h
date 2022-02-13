4c25b0da127bed2a81a4a88845bab6052df7cddb
Garrett Brown <themagnificentmrb@gmail.com>
2022-02-12 23:00:47 -0800

    VFS API v4.0.0: Add Content-Addressable Storage functionality (WIP)

--- xbmc/addons/kodi-dev-kit/include/kodi/versions.h
+++ xbmc/addons/kodi-dev-kit/include/kodi/versions.h
@@ -162,6 +162,7 @@
 #define ADDON_INSTANCE_VERSION_SCREENSAVER_DEPENDS    "c-api/addon-instance/screensaver.h" \
                                                       "addon-instance/Screensaver.h"
 
+//! @todo Change VFS version to "4.0.0"
 #define ADDON_INSTANCE_VERSION_VFS                    "3.0.1"
 #define ADDON_INSTANCE_VERSION_VFS_MIN                "3.0.1"
 #define ADDON_INSTANCE_VERSION_VFS_XML_ID             "kodi.binary.instance.vfs"
