a780e4a8ea9741c75b5fe1ece0a69dc3fb3ad9cb
Lukas Rusak <lorusak@gmail.com>
2022-11-23 19:53:09 -0800

    [PR 22349] CRPProccessInfo: make CreateInstance return a unique_ptr

--- xbmc/cores/RetroPlayer/process/RPProcessInfo.cpp
+++ xbmc/cores/RetroPlayer/process/RPProcessInfo.cpp
@@ -65,9 +65,9 @@ CRPProcessInfo::CRPProcessInfo(std::string platformName)
 
 CRPProcessInfo::~CRPProcessInfo() = default;
 
-CRPProcessInfo* CRPProcessInfo::CreateInstance()
+std::unique_ptr<CRPProcessInfo> CRPProcessInfo::CreateInstance()
 {
-  CRPProcessInfo* processInfo = nullptr;
+  std::unique_ptr<CRPProcessInfo> processInfo;
 
   std::unique_lock<CCriticalSection> lock(m_createSection);
 
@@ -75,7 +75,7 @@ CRPProcessInfo* CRPProcessInfo::CreateInstance()
   {
     processInfo = m_processControl();
 
-    if (processInfo != nullptr)
+    if (processInfo)
       CLog::Log(LOGINFO, "RetroPlayer[PROCESS]: Created process info for {}",
                 processInfo->GetPlatformName());
     else
