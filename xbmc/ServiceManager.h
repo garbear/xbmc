a13918e98106a6ed7f1bee391e53af5240800b08
Garrett Brown <themagnificentmrb@gmail.com>
2023-02-01 16:27:32 -0800

    [WIP] RetroEngine: A new player core for kodi
    This includes two subsystems:
    
      * Rendering
      * Input

--- xbmc/ServiceManager.h
+++ xbmc/ServiceManager.h
@@ -67,6 +67,11 @@ namespace RETRO
 {
 class CGUIGameRenderManager;
 }
+
+namespace RETRO_ENGINE
+{
+class CRetroEngineServices;
+}
 } // namespace KODI
 
 namespace MEDIA_DETECT
@@ -149,6 +154,8 @@ public:
   MEDIA_DETECT::CDetectDVDMedia& GetDetectDVDMedia();
 #endif
 
+  KODI::RETRO_ENGINE::CRetroEngineServices& GetRetroEngineServices();
+
 protected:
   std::unique_ptr<ADDON::CAddonMgr> m_addonMgr;
   std::unique_ptr<ADDON::CBinaryAddonManager> m_binaryAddonManager;
@@ -171,6 +178,7 @@ protected:
   std::unique_ptr<KODI::GAME::CControllerManager> m_gameControllerManager;
   std::unique_ptr<KODI::GAME::CGameServices> m_gameServices;
   std::unique_ptr<KODI::RETRO::CGUIGameRenderManager> m_gameRenderManager;
+  std::unique_ptr<KODI::RETRO_ENGINE::CRetroEngineServices> m_retroEngineServices;
   std::unique_ptr<PERIPHERALS::CPeripherals> m_peripherals;
   std::unique_ptr<CFavouritesService> m_favouritesService;
   std::unique_ptr<CInputManager> m_inputManager;
