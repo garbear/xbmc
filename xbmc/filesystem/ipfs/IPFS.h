b5e1befc8d2e4b5d2d84793caacb48585e298b90
Garrett Brown <themagnificentmrb@gmail.com>
2023-10-08 10:59:52 -0700

    IPFS WIP

--- xbmc/filesystem/ipfs/IPFS.h
+++ xbmc/filesystem/ipfs/IPFS.h
@@ -34,18 +34,8 @@ public:
   bool Initialize(const std::string& dataStoreRoot) override;
   void Deinitialize() override;
   bool IsOnline() override;
-  std::string ResolveName(const std::string& identifier) override;
-  void PublishName(const std::string& ipfsPath,
-                   unsigned int lifetimeSecs,
-                   unsigned int ttlSecs,
-                   const std::string& keyName) override;
   CVariant GetDAG(const std::string& cid) override;
   std::string PutDAG(const CVariant& content) override;
-  std::vector<std::string> ListKeys() override;
-  void RemoveKey(const std::string& keyName) override;
-  void ImportKey(const std::string& keyName,
-                 const KODI::CRYPTO::PrivateKey& privateKey,
-                 const std::string& password) override;
 
 private:
   std::unique_ptr<KODI::DATASTORE::CDataStore> m_dataStore;
