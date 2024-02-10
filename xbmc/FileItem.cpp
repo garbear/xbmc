650262a120880da7e1cfd96b7a7ffec335278244
Arne Morten Kvarving <spiff@kodi.tv>
2024-02-11 00:59:50 +0100

    [PR 25009] move CFileItem::IsSubtitle to VideoFileItemClassify

--- xbmc/FileItem.cpp
+++ xbmc/FileItem.cpp
@@ -1020,11 +1020,6 @@ bool CFileItem::IsLyrics() const
   return URIUtils::HasExtension(m_strPath, ".cdg|.lrc");
 }
 
-bool CFileItem::IsSubtitle() const
-{
-  return URIUtils::HasExtension(m_strPath, CServiceBroker::GetFileExtensionProvider().GetSubtitleExtensions());
-}
-
 bool CFileItem::IsCUESheet() const
 {
   return URIUtils::HasExtension(m_strPath, ".cue");
