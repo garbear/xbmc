c1269467d0f0780f7587ae3555bf156c43401316
Arne Morten Kvarving <spiff@kodi.tv>
2024-02-11 00:59:50 +0100

    [PR 25029] move CFileItem::IsLyrics to MusicFileItemClassify

--- xbmc/FileItem.cpp
+++ xbmc/FileItem.cpp
@@ -1013,11 +1013,6 @@ bool CFileItem::IsPicture() const
   return false;
 }
 
-bool CFileItem::IsLyrics() const
-{
-  return URIUtils::HasExtension(m_strPath, ".cdg|.lrc");
-}
-
 bool CFileItem::IsInternetStream(const bool bStrictCheck /* = false */) const
 {
   if (HasProperty("IsHTTPDirectory"))
