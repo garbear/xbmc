c3e611c90713d14a20f7513bdd27074fa9758c23
Garrett Brown <themagnificentmrb@gmail.com>
2024-03-09 04:38:56 -0800

    [PR 24501] [macOS] Fix compile on macOS
    Error was:
    
      [ 93%] Building CXX object build/platform/osx/SDL/CMakeFiles/platform_osx_SDL.dir/OSXTextInputResponder.mm.o
      platform/darwin/osx/SDL/OSXTextInputResponder.mm:26:25: error: allocation of incomplete type 'CAction'
    
      CAction *action = new CAction(ACTION_INPUT_TEXT);
                            ^~~~~~~

--- xbmc/platform/darwin/osx/SDL/OSXTextInputResponder.mm
+++ xbmc/platform/darwin/osx/SDL/OSXTextInputResponder.mm
@@ -11,6 +11,8 @@
 #include "GUIUserMessages.h"
 #include "ServiceBroker.h"
 #include "guilib/GUIWindowManager.h"
+#include "input/actions/Action.h"
+#include "input/actions/ActionIDs.h"
 #include "input/keyboard/Key.h"
 #include "messaging/ApplicationMessenger.h"
 #include "utils/log.h"
