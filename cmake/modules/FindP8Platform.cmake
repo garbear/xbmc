45c6e2dec77c7d6423befc169b0ee7bfbb6cc2e7
Philipp Kerling <yol@casix.org>
2024-03-30 07:17:15 +0100

    [depends] Incorporate p8-platform PR #46

--- cmake/modules/FindP8Platform.cmake
+++ cmake/modules/FindP8Platform.cmake
@@ -14,7 +14,8 @@ if(NOT TARGET ${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME} OR P8Platform_FIND_REQU
   macro(buildlibp8platform)
     set(patches "${CORE_SOURCE_DIR}/tools/depends/target/${MODULE_LC}/001-all-fix-c++17-support.patch"
                 "${CORE_SOURCE_DIR}/tools/depends/target/${MODULE_LC}/002-all-fixcmakeinstall.patch"
-                "${CORE_SOURCE_DIR}/tools/depends/target/${MODULE_LC}/003-all-cmake_tweakversion.patch")
+                "${CORE_SOURCE_DIR}/tools/depends/target/${MODULE_LC}/003-all-cmake_tweakversion.patch"
+                "${CORE_SOURCE_DIR}/tools/depends/target/${MODULE_LC}/004-all-fix-cxx-standard.patch")
 
     generate_patchcommand("${patches}")
 
