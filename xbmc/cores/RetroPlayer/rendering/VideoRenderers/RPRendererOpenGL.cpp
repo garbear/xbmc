f91fd51c72a3474134409478a075428046a83689
KOPRajs <zdenek.koprivik@post.cz>
2024-04-14 14:00:24 +0200

    Remove renderer code not needed for video shaders from common path

--- xbmc/cores/RetroPlayer/rendering/VideoRenderers/RPRendererOpenGL.cpp
+++ xbmc/cores/RetroPlayer/rendering/VideoRenderers/RPRendererOpenGL.cpp
@@ -273,15 +273,6 @@ void CRPRendererOpenGL::Render(uint8_t alpha)
   if (renderBuffer == nullptr)
     return;
 
-  CRect rect = m_sourceRect;
-
-  rect.x1 /= renderBuffer->GetWidth();
-  rect.x2 /= renderBuffer->GetWidth();
-  rect.y1 /= renderBuffer->GetHeight();
-  rect.y2 /= renderBuffer->GetHeight();
-
-  const uint32_t color = (alpha << 24) | 0xFFFFFF;
-
   RenderBufferTextures* rbTextures;
   const auto it = m_RBTexturesMap.find(renderBuffer);
   if (it != m_RBTexturesMap.end())
@@ -311,17 +302,6 @@ void CRPRendererOpenGL::Render(uint8_t alpha)
   const auto sourceTexture = &rbTextures->source;
   const auto targetTexture = &rbTextures->target;
 
-  glBindTexture(m_textureTarget, sourceTexture->getMTexture());
-
-  GLint filter = GL_NEAREST;
-  if (GetRenderSettings().VideoSettings().GetScalingMethod() == SCALINGMETHOD::LINEAR)
-    filter = GL_LINEAR;
-
-  glTexParameteri(m_textureTarget, GL_TEXTURE_MAG_FILTER, filter);
-  glTexParameteri(m_textureTarget, GL_TEXTURE_MIN_FILTER, filter);
-  glTexParameteri(m_textureTarget, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-  glTexParameteri(m_textureTarget, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-
   Updateshaders();
 
   if (m_bUseShaderPreset)
@@ -341,6 +321,26 @@ void CRPRendererOpenGL::Render(uint8_t alpha)
   }
   else
   {
+    CRect rect = m_sourceRect;
+
+    rect.x1 /= renderBuffer->GetWidth();
+    rect.x2 /= renderBuffer->GetWidth();
+    rect.y1 /= renderBuffer->GetHeight();
+    rect.y2 /= renderBuffer->GetHeight();
+
+    const uint32_t color = (alpha << 24) | 0xFFFFFF;
+
+    glBindTexture(m_textureTarget, sourceTexture->getMTexture());
+
+    GLint filter = GL_NEAREST;
+    if (GetRenderSettings().VideoSettings().GetScalingMethod() == SCALINGMETHOD::LINEAR)
+      filter = GL_LINEAR;
+
+    glTexParameteri(m_textureTarget, GL_TEXTURE_MAG_FILTER, filter);
+    glTexParameteri(m_textureTarget, GL_TEXTURE_MIN_FILTER, filter);
+    glTexParameteri(m_textureTarget, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(m_textureTarget, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
     m_context.EnableGUIShader(GL_SHADER_METHOD::TEXTURE);
 
     GLubyte colour[4];
