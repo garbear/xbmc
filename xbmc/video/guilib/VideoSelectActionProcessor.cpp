30f684c121b6ea8144b47776260c4e1eebc4de6c
ksooo <3226626+ksooo@users.noreply.github.com>
2024-05-29 21:44:17 +0200

    [PR 25276] [video] VideoSelectActionProcessor: On ACTION_INFO, if the item has no video db info data, fall back to resume/play.

--- xbmc/video/guilib/VideoSelectActionProcessor.cpp
+++ xbmc/video/guilib/VideoSelectActionProcessor.cpp
@@ -21,6 +21,7 @@
 #include "settings/SettingsComponent.h"
 #include "utils/StringUtils.h"
 #include "utils/Variant.h"
+#include "video/VideoFileItemClassify.h"
 #include "video/VideoInfoTag.h"
 #include "video/guilib/VideoGUIUtils.h"
 
@@ -69,7 +70,17 @@ bool CVideoSelectActionProcessorBase::Process(Action action)
       return OnQueueSelected();
 
     case ACTION_INFO:
+    {
+      if (GetDefaultAction() == ACTION_INFO && !KODI::VIDEO::IsVideoDb(*m_item) &&
+          !m_item->IsPlugin() && !m_item->IsScript() &&
+          !VIDEO_UTILS::HasItemVideoDbInformation(*m_item))
+      {
+        // for items without info fall back to default play action
+        return Process(CVideoPlayActionProcessorBase::GetDefaultAction());
+      }
+
       return OnInfoSelected();
+    }
 
     case ACTION_MORE:
       return OnMoreSelected();
