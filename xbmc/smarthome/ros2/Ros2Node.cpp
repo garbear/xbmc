ecc1d8b6db38db4eef2e5ee041ec1d5600ec3720
Garrett Brown <themagnificentmrb@gmail.com>
2024-06-10 18:24:28 -0700

    Smart Home: Add system health subscriber

--- xbmc/smarthome/ros2/Ros2Node.cpp
+++ xbmc/smarthome/ros2/Ros2Node.cpp
@@ -9,6 +9,7 @@
 #include "Ros2Node.h"
 
 #include "Ros2InputPublisher.h"
+#include "Ros2SystemHealthManager.h"
 #include "Ros2VideoSubscription.h"
 #include "ServiceBroker.h"
 #include "network/Network.h"
@@ -23,6 +24,8 @@ using namespace SMART_HOME;
 
 namespace
 {
+constexpr const char* ROS_NAMESPACE = "oasis"; //! @todo
+
 // Name of the ROS node (hostname is concatenated at the end)
 constexpr const char* NODE_NAME_PREFIX = "kodi_"; //! @todo Get from version.txt
 
@@ -33,7 +36,9 @@ constexpr const char* THREAD_NAME = "ROS2"; // TODO
 constexpr const char* HOSTNAME_UNKNOWN = "unknown";
 } // namespace
 
-CRos2Node::CRos2Node(CSmartHomeInputManager& inputManager) : m_inputManager(inputManager)
+CRos2Node::CRos2Node(CSmartHomeInputManager& inputManager)
+  : m_inputManager(inputManager),
+    m_systemHealthManager(std::make_unique<CRos2SystemHealthManager>(ROS_NAMESPACE))
 {
 }
 
@@ -48,6 +53,9 @@ void CRos2Node::Initialize()
   // Can't make virtual calls from constructor
   m_node = std::make_shared<rclcpp::Node>(NODE_NAME_PREFIX + hostname);
   m_thread = std::make_unique<CThread>(this, THREAD_NAME);
+
+  // Managers
+  m_systemHealthManager->Initialize(m_node);
 
   // Publishers
   m_peripheralPublisher =
@@ -66,6 +74,8 @@ void CRos2Node::Deinitialize()
   // Stop thread
   if (m_thread)
     m_thread->StopThread(true);
+
+  m_systemHealthManager->Deinitialize();
 
   for (const auto& videoSub : m_videoSubs)
     videoSub.second->Deinitialize();
@@ -96,6 +106,11 @@ void CRos2Node::UnregisterImageTopic(const std::string& topic)
   auto it = m_videoSubs.find(topic);
   if (it != m_videoSubs.end())
     m_videoSubs.erase(it);
+}
+
+ISystemHealthHUD* CRos2Node::GetSystemHealthHUD() const
+{
+  return m_systemHealthManager.get();
 }
 
 void CRos2Node::FrameMove()
