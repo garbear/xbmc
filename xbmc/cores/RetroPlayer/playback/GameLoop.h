692fd64f3f1882925087f6ae8f7324b84c88eb99
Garrett Brown <themagnificentmrb@gmail.com>
2024-08-10 16:34:25 -0700

    [WIP PR 25460] RetroPlayer: Several improvements
    * Document CGameLoop and IGameLoopCallback
    * Improve initialization of member variables
    * Break += statement into two lines

--- xbmc/cores/RetroPlayer/playback/GameLoop.h
+++ xbmc/cores/RetroPlayer/playback/GameLoop.h
@@ -18,6 +18,15 @@ namespace KODI
 {
 namespace RETRO
 {
+/*!
+ * \brief Interface for the game loop callback
+ *
+ * The IGameLoopCallback interface provides the necessary methods for handling
+ * frame events within the game loop.
+ *
+ * Implementers must define how the next frame (FrameEvent) and previous frame
+ * (RewindEvent) are processed.
+ */
 class IGameLoopCallback
 {
 public:
@@ -34,6 +43,17 @@ public:
   virtual void RewindEvent() = 0;
 };
 
+/*!
+ * \brief The game loop class
+ *
+ * CGameLoop is responsible for managing the timing and execution of frame
+ * updates in a game or emulator.
+ *
+ * Inside the Process method, the loop checks for changes in the speed factor,
+ * updates frame timing, and uses precise sleep intervals to maintain a
+ * consistent frame rate while invoking the appropriate callbacks for frame
+ * events.
+ */
 class CGameLoop : protected CThread
 {
 public:
@@ -60,8 +80,8 @@ private:
 
   IGameLoopCallback* const m_callback;
   const double m_fps;
-  std::atomic<double> m_speedFactor;
-  double m_loopSpeedFactor{0};
+  std::atomic<double> m_speedFactor{0.0};
+  double m_loopSpeedFactor{0.0};
   std::chrono::microseconds m_lastFrameUs{std::chrono::microseconds::zero()};
   CEvent m_sleepEvent;
 };
