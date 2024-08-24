49b07c1154fc46a408475bbda58a3da0af6c2123
Garrett Brown <themagnificentmrb@gmail.com>
2024-08-24 09:01:50 -0700

    Revert "[PR 25611] [video][windows] Fix videos provided by plugins not played from info dialog for certain settings combination."
    This reverts commit 210c64facecfe2527decb5a6bc65b2b9fe2d7b31.

--- xbmc/video/VideoUtils.cpp
+++ xbmc/video/VideoUtils.cpp
@@ -133,15 +133,12 @@ void CAsyncGetItemsForPlaylist::GetItemsForPlaylist(const std::shared_ptr<CFileI
 
   if (item->m_bIsFolder)
   {
-    if (!item->IsPlugin())
+    // check if it's a folder with dvd or bluray files, then just add the relevant file
+    const std::string mediapath = item->GetOpticalMediaPath();
+    if (!mediapath.empty())
     {
-      // check if it's a folder with dvd or bluray files, then just add the relevant file
-      const std::string mediapath = item->GetOpticalMediaPath();
-      if (!mediapath.empty())
-      {
-        m_queuedItems.Add(std::make_shared<CFileItem>(mediapath, false));
-        return;
-      }
+      m_queuedItems.Add(std::make_shared<CFileItem>(mediapath, false));
+      return;
     }
 
     // Check if we add a locked share
