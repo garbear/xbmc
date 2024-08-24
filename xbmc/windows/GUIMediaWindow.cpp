a345442e990757a83aecf6af8c9c85847943ad90
Garrett Brown <themagnificentmrb@gmail.com>
2024-08-24 09:01:50 -0700

    Revert "[PR 25611] [video][windows] Fix videos provided by plugins not played from info dialog for certain settings combination."
    This reverts commit 210c64facecfe2527decb5a6bc65b2b9fe2d7b31.

--- xbmc/windows/GUIMediaWindow.cpp
+++ xbmc/windows/GUIMediaWindow.cpp
@@ -767,12 +767,6 @@ bool CGUIMediaWindow::GetDirectory(const std::string &strDirectory, CFileItemLis
       m_history.RemoveParentPath();
   }
 
-  // Store parent path along with item as parent path cannot safely be calculated from item's path.
-  for (const auto& item : items)
-  {
-    item->SetProperty("ParentPath", m_vecItems->GetPath());
-  }
-
   // update the view state's reference to the current items
   m_guiState.reset(CGUIViewState::GetViewState(GetID(), items));
 
@@ -1761,6 +1755,8 @@ bool CGUIMediaWindow::OnPopupMenu(int itemIdx)
   auto item = m_vecItems->Get(itemIdx);
   if (!item)
     return false;
+
+  item->SetProperty("ParentPath", m_vecItems->GetPath());
 
   CContextButtons buttons;
 
