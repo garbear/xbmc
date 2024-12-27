77e3ff50ccb06b25677b8a6869041b1a3cff383d
Arne Morten Kvarving <spiff@kodi.tv>
2024-12-27 18:27:47 +0100

    [PR 26207] XFILE::IOControl: convert to enum class

--- xbmc/addons/interfaces/Filesystem.cpp
+++ xbmc/addons/interfaces/Filesystem.cpp
@@ -1003,9 +1003,8 @@ bool Interface_Filesystem::io_control_get_seek_possible(void* kodiBase, void* fi
     return false;
   }
 
-  return static_cast<CFile*>(file)->IoControl(EIoControl::IOCTRL_SEEK_POSSIBLE, nullptr) != 0
-             ? true
-             : false;
+  return static_cast<CFile*>(file)->IoControl(IOControl::SEEK_POSSIBLE, nullptr) != 0 ? true
+                                                                                      : false;
 }
 
 bool Interface_Filesystem::io_control_get_cache_status(void* kodiBase,
@@ -1021,7 +1020,7 @@ bool Interface_Filesystem::io_control_get_cache_status(void* kodiBase,
   }
 
   SCacheStatus data = {};
-  int ret = static_cast<CFile*>(file)->IoControl(EIoControl::IOCTRL_CACHE_STATUS, &data);
+  int ret = static_cast<CFile*>(file)->IoControl(IOControl::CACHE_STATUS, &data);
   if (ret >= 0)
   {
     status->forward = data.forward;
@@ -1043,8 +1042,7 @@ bool Interface_Filesystem::io_control_set_cache_rate(void* kodiBase, void* file,
     return false;
   }
 
-  return static_cast<CFile*>(file)->IoControl(EIoControl::IOCTRL_CACHE_SETRATE, &rate) >= 0 ? true
-                                                                                            : false;
+  return static_cast<CFile*>(file)->IoControl(IOControl::CACHE_SETRATE, &rate) >= 0 ? true : false;
 }
 
 bool Interface_Filesystem::io_control_set_retry(void* kodiBase, void* file, bool retry)
@@ -1057,8 +1055,7 @@ bool Interface_Filesystem::io_control_set_retry(void* kodiBase, void* file, bool
     return false;
   }
 
-  return static_cast<CFile*>(file)->IoControl(EIoControl::IOCTRL_SET_RETRY, &retry) >= 0 ? true
-                                                                                         : false;
+  return static_cast<CFile*>(file)->IoControl(IOControl::SET_RETRY, &retry) >= 0 ? true : false;
 }
 
 char** Interface_Filesystem::get_property_values(
