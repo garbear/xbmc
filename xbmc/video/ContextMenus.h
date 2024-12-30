99e685216d231f89e8b2be73a707d1cf432ee1de
Garrett Brown <themagnificentmrb@gmail.com>
2024-12-30 08:43:53 -0800

    Revert "[PR 26172] [video] Add CONTEXTMENU::CVideoInfo to support items with media type 'video' and non-empty video items with empty media type."
    This reverts commit a6197677b5bdf05a969eea5609b9fb68cc76efa8.

--- xbmc/video/ContextMenus.h
+++ xbmc/video/ContextMenus.h
@@ -28,12 +28,6 @@ private:
   const MediaType m_mediaType;
 };
 
-struct CVideoInfo : CVideoInfoBase
-{
-  CVideoInfo() : CVideoInfoBase(MediaTypeVideo) {}
-  bool IsVisible(const CFileItem& item) const override;
-};
-
 struct CTVShowInfo : CVideoInfoBase
 {
   CTVShowInfo() : CVideoInfoBase(MediaTypeTvShow) {}
