d2b5a15327bfe1e97a50e917261ced4d9170ee21
Garrett Brown <themagnificentmrb@gmail.com>
2025-01-18 17:32:03 -0800

    [PR 26315] [guilib] Fix game controller aspect ratio

--- xbmc/games/controllers/guicontrols/GUIGameController.cpp
+++ xbmc/games/controllers/guicontrols/GUIGameController.cpp
@@ -16,6 +16,7 @@
 #include "games/controllers/Controller.h"
 #include "games/controllers/ControllerLayout.h"
 #include "guilib/GUIListItem.h"
+#include "guilib/GUITexture.h"
 #include "utils/StringUtils.h"
 #include "utils/log.h"
 
@@ -37,6 +38,9 @@ CGUIGameController::CGUIGameController(int parentID,
 {
   // Initialize CGUIControl
   ControlType = GUICONTROL_GAMECONTROLLER;
+
+  // Initialize CGUIImage
+  SetAspectRatio(CAspectRatio::AR_KEEP);
 }
 
 CGUIGameController::CGUIGameController(const CGUIGameController& from)
@@ -52,6 +56,9 @@ CGUIGameController::CGUIGameController(const CGUIGameController& from)
 {
   // Initialize CGUIControl
   ControlType = GUICONTROL_GAMECONTROLLER;
+
+  // Initialize CGUIImage
+  SetAspectRatio(CAspectRatio::AR_KEEP);
 }
 
 CGUIGameController* CGUIGameController::Clone(void) const
