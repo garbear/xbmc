40d6863e86402f354170816f48fa6fb1045f698b
KOPRajs <zdenek.koprivik@post.cz>
2025-01-27 08:00:24 +0100

    Fix shaders for macOS

--- xbmc/cores/RetroPlayer/shaders/gl/ShaderUtilsGL.cpp
+++ xbmc/cores/RetroPlayer/shaders/gl/ShaderUtilsGL.cpp
@@ -120,9 +120,7 @@ std::string CShaderUtilsGL::GetGLSLVersion(std::string& source)
     else
       versionString = std::to_string(version);
 
-    // Do not force GLSL version for OpenGL
-    //versionString = "#version " + versionString + "\n";
-    versionString = "\n";
+    versionString = "#version " + versionString + "\n";
 #else
     if (version == 200)
       versionString = "100";
