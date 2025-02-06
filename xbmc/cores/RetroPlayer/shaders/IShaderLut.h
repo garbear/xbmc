6e94c2840939dcb8b0da9d3578b9295ef5fb0921
Garrett Brown <themagnificentmrb@gmail.com>
2025-02-05 23:10:17 -0800

    Clean up and improve documentation

--- xbmc/cores/RetroPlayer/shaders/IShaderLut.h
+++ xbmc/cores/RetroPlayer/shaders/IShaderLut.h
@@ -39,26 +39,31 @@ public:
 
   /*!
    * \brief Create the LUT and allocate resources
+   *
    * \param context The render context
    * \param lut The LUT information structure
+   *
    * \return Returns true if successful and the LUT can be used, false otherwise
    */
   virtual bool Create(RETRO::CRenderContext& context, const ShaderLut& lut) = 0;
 
   /*!
    * \brief Gets ID of LUT
+   *
    * \return Returns unique name (ID) of look up texture
    */
   const std::string& GetID() const { return m_id; }
 
   /*!
    * \brief Gets full path of LUT
+   *
    * \return Returns full path of look up texture
    */
   const std::string& GetPath() const { return m_path; }
 
   /*!
    * \brief Gets texture of LUT
+   *
    * \return Returns pointer to the texture where the LUT data is stored in
    */
   virtual CTexture* GetTexture() = 0;
