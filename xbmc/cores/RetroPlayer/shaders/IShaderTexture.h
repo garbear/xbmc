184a76c81c88eb642f7e624dabea9d1eed5557c6
Garrett Brown <themagnificentmrb@gmail.com>
2025-02-05 23:10:17 -0800

    Clean up and improve documentation

--- xbmc/cores/RetroPlayer/shaders/IShaderTexture.h
+++ xbmc/cores/RetroPlayer/shaders/IShaderTexture.h
@@ -12,6 +12,12 @@ namespace KODI
 {
 namespace SHADER
 {
+/*!
+ * \brief Interface for a shader texture
+ *
+ * The interface is left relatively generic. The implementation should be able
+ * to manage any resources required by the texture.
+ */
 class IShaderTexture
 {
 public:
@@ -19,12 +25,14 @@ public:
 
   /*!
    * \brief Return width of texture
+   *
    * \return Width of the texture in texels
    */
   virtual float GetWidth() const = 0;
 
   /*!
    * \brief Return height of texture
+   *
    * \return Height of the texture in texels
    */
   virtual float GetHeight() const = 0;
