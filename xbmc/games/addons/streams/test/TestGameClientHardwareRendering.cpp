c2a28c929052484be57f74c84e62d4e35072abbf
Garrett Brown <themagnificentmrb@gmail.com>
2026-06-22 21:08:33 -0700

    RetroPlayer: Compress savestate blobs with zstd

--- xbmc/games/addons/streams/test/TestGameClientHardwareRendering.cpp
+++ xbmc/games/addons/streams/test/TestGameClientHardwareRendering.cpp
@@ -507,7 +507,7 @@ protected:
     file->Close();
     RETRO::CSavestateFlatBuffer savestate;
     *savestate.GetMemoryBuffer(1) = 1;
-    savestate.Finalize();
+    savestate.Finalize(true);
     RETRO::CSavestateDatabase database;
     ASSERT_TRUE(database.AddSavestate(path, {}, savestate));
     {
